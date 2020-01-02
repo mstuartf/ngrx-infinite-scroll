8d76f60c95df9c945a86e0ba82d945e058494bba
mikef <mstuartflood@gmail.com>
2020-01-02 20:31:09 +0000

    add prettier to project

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -1,25 +1,22 @@
-import {Component, OnInit} from '@angular/core';
-import { Store, select } from '@ngrx/store';
-import { AppState } from '../state/state';
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { Component } from "@angular/core";
+import { select, Store } from "@ngrx/store";
+import { AppState } from "../state/state";
+import { Observable } from "rxjs";
 import { nextPageRequest } from "../../state/actions";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
-
   public people$: Observable<number>;
 
   constructor(private store$: Store<AppState>) {
-    this.people$ = this.store$.pipe(select<AppState, 'people'>('people'));
+    this.people$ = this.store$.pipe(select<AppState, "people">("people"));
     this.nextPage();
   }
 
   public nextPage(): void {
     this.store$.dispatch(nextPageRequest());
   }
-
 }
--- state/effects.ts
+++ state/effects.ts
@@ -1,34 +1,29 @@
-import {Injectable} from '@angular/core';
-import {Actions, createEffect, ofType} from '@ngrx/effects';
-import {map, mergeMap, switchMap} from 'rxjs/operators';
-import {HttpClient} from '@angular/common/http';
-import {EMPTY} from 'rxjs';
-import {catchError, tap, withLatestFrom} from 'rxjs/operators';
-import { nextPageSuccess, nextPageFailure } from "./actions";
-import { Store, select } from '@ngrx/store';
-import { AppState } from '../app/state';
+import { Injectable } from "@angular/core";
+import { Actions, createEffect, ofType } from "@ngrx/effects";
+import { catchError, map, switchMap, withLatestFrom } from "rxjs/operators";
+import { HttpClient } from "@angular/common/http";
+import { nextPageFailure, nextPageSuccess } from "./actions";
+import { select, Store } from "@ngrx/store";
+import { AppState } from "./state";
 
 @Injectable()
 export class PeopleEffects {
+  nextPage$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType("[Home Page] Next Page Request"),
+      withLatestFrom(this.store$.pipe(select<AppState, "people">("people"))),
+      switchMap(([action, state]) => {
+        return this.http$.get(state.next).pipe(
+          map(resp => nextPageSuccess({ list: resp.results, next: resp.next })),
+          catchError(() => nextPageFailure())
+        );
+      })
+    )
+  );
 
-    nextPage$ = createEffect(() => this.actions$.pipe(
-      ofType('[Home Page] Next Page Request'),
-      withLatestFrom(this.store$.pipe(select<AppState, 'people'>('people'))),
-        switchMap(([action, state]) => {
-          // console.log(state.next);
-          return this.http$.get(state.next).pipe(
-            map((resp) => nextPageSuccess({list: resp.results, next: resp.next})),
-            catchError(() => nextPageFailure())
-        )
-        }),
-        )
-    );
-
-    constructor(
-      private store$: Store<AppState>,
-        private actions$: Actions,
-        private http$: HttpClient,
-    ) {
-    }
-
+  constructor(
+    private store$: Store<AppState>,
+    private actions$: Actions,
+    private http$: HttpClient
+  ) {}
 }
